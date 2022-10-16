735482f2671e7e7075ba6d864cec1fb611b729ad
Ashokchakraa <acdhulipalla@gmail.com>
2022-10-16 15:30:15 +0530

    changes

--- py1.py
+++ py1.py
@@ -8,3 +8,46 @@ d = 20
 add = a + b
 
 v = c + d
+
+def sqvare (f):
+    def innar(a, b):
+        a = a*a
+        b = b*b
+        return f(a, b)
+    return innar    
+
+
+@sqvare
+
+def add (a, b):
+   return a + b
+print(add(5, 3))
+
+@sqvare
+
+def add (a, b):
+   return a * b
+print(add(2, 3))
+
+
+
+
+
+def check_denom(f):
+   def innar(a, b):
+      if b != 0:
+         return f(a, b)
+      else:
+         return 'b value should not be 0'
+   return innar 
+@check_denom         
+def dir (a, b):
+   return a / b
+print(dir(10, 5)) 
+print(dir(0, 2))  
+print(dir(2, 0))  # ZeroDivisionError: division by zero
+
+
+
+
+
